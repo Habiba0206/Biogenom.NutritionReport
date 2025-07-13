eacef1695cd1b8794d3b929edd4c27a24a600a77
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:25:16 +0500

    created benefit queries

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Application/Benefits/Queries/BenefitGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using Biogenom.NutritionReport.Application.Benefits.Models;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+
+namespace Biogenom.NutritionReport.Application.Benefits.Queries;
+
+public record BenefitGetByIdQuery : IQuery<BenefitGetDto?>
+{
+    public Guid BenefitId { get; set; }
+}
---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Application/Benefits/Queries/BenefitGetQuery.cs
@@ -0,0 +1,9 @@
+﻿using Biogenom.NutritionReport.Application.Benefits.Models;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+
+namespace Biogenom.NutritionReport.Application.Benefits.Queries;
+
+public record BenefitGetQuery : IQuery<ICollection<BenefitGetDto>>
+{
+    public BenefitFilter BenefitFilter { get; set; }
+}
