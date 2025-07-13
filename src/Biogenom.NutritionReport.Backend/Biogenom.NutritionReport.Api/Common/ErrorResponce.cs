d8d0931620ec371bdc54e35d4b83a24362840574
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 17:56:52 +0500

    created error responce

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Api/Common/ErrorResponce.cs
@@ -0,0 +1,8 @@
+﻿namespace Biogenom.NutritionReport.Api.Common;
+
+public class ErrorResponse
+{
+    public string Error { get; set; } = default!;
+    public List<string>? Details { get; set; }
+}
+
