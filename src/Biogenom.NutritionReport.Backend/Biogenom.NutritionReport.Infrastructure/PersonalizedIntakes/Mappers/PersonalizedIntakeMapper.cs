ed43b8604550672d7d1438236f82e55508309e9f
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:45:29 +0500

    created PI mapper

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Infrastructure/PersonalizedIntakes/Mappers/PersonalizedIntakeMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Biogenom.NutritionReport.Application.PersonalizedIntakes.Models;
+using Biogenom.NutritionReport.Domain.Entities;
+
+namespace Biogenom.NutritionReport.Infrastructure.PersonalizedIntakes.Mappers;
+
+public class PersonalizedIntakeMapper : Profile
+{
+    public PersonalizedIntakeMapper()
+    {
+        CreateMap<PersonalizedIntake, PersonalizedIntakeCreateUpdateDto>().ReverseMap();
+        CreateMap<PersonalizedIntake, PersonalizedIntakeGetDto>().ReverseMap();
+        CreateMap<PersonalizedIntake, PersonalizedIntakePatchDto>().ReverseMap();
+    }
+}
