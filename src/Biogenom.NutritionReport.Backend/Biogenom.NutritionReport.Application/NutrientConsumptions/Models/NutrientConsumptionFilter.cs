051d01129e758717b87f364dc3c13ec9f7414f1f
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:29:59 +0500

    created NC filter

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Application/NutrientConsumptions/Models/NutrientConsumptionFilter.cs
@@ -0,0 +1,17 @@
+﻿using Biogenom.NutritionReport.Domain.Common.Queries;
+
+namespace Biogenom.NutritionReport.Application.NutrientConsumptions.Models;
+
+public class NutrientConsumptionFilter : FilterPagination
+{
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+        hashCode.Add(PageToken);
+        hashCode.Add(PageSize);
+        return hashCode.ToHashCode();
+    }
+
+    public override bool Equals(object? obj) =>
+        obj is NutrientConsumptionFilter filter && filter.GetHashCode() == GetHashCode();
+}
