9b3b521c9e9bbc045ee99284cced55f67c18c3c7
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:30:10 +0500

    created NC queries

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Application/NutrientConsumptions/Queries/NutrientConsumptionGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using Biogenom.NutritionReport.Application.NutrientConsumptions.Models;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+
+namespace Biogenom.NutritionReport.Application.NutrientConsumptions.Queries;
+
+public record NutrientConsumptionGetByIdQuery : IQuery<NutrientConsumptionGetDto?>
+{
+    public Guid NutrientConsumptionId { get; set; }
+}
---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Application/NutrientConsumptions/Queries/NutrientConsumptionGetQuery.cs
@@ -0,0 +1,9 @@
+﻿using Biogenom.NutritionReport.Application.NutrientConsumptions.Models;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+
+namespace Biogenom.NutritionReport.Application.NutrientConsumptions.Queries;
+
+public record NutrientConsumptionGetQuery : IQuery<ICollection<NutrientConsumptionGetDto>>
+{
+    public NutrientConsumptionFilter NutrientConsumptionFilter { get; set; }
+}
