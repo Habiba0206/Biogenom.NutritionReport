912f034fcd7d52f2e641a8c58eb24a16a34d1bc2
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:45:39 +0500

    created PI query handlers

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Infrastructure/PersonalizedIntakes/QueryHandlers/PersonalizedIntakeGetByIdQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Biogenom.NutritionReport.Application.PersonalizedIntakes.Models;
+using Biogenom.NutritionReport.Application.PersonalizedIntakes.Queries;
+using Biogenom.NutritionReport.Application.PersonalizedIntakes.Services;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+
+namespace Biogenom.NutritionReport.Infrastructure.PersonalizedIntakes.QueryHandlers;
+
+public class PersonalizedIntakeGetByIdQueryHandler(
+    IMapper mapper,
+    IPersonalizedIntakeService service)
+    : IQueryHandler<PersonalizedIntakeGetByIdQuery, PersonalizedIntakeGetDto?>
+{
+    public async Task<PersonalizedIntakeGetDto?> Handle(PersonalizedIntakeGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var entity = await service.GetByIdAsync(request.PersonalizedIntakeId, cancellationToken: cancellationToken);
+        return mapper.Map<PersonalizedIntakeGetDto?>(entity);
+    }
+}
---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Infrastructure/PersonalizedIntakes/QueryHandlers/PersonalizedIntakeGetQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Biogenom.NutritionReport.Application.PersonalizedIntakes.Models;
+using Biogenom.NutritionReport.Application.PersonalizedIntakes.Queries;
+using Biogenom.NutritionReport.Application.PersonalizedIntakes.Services;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+using Biogenom.NutritionReport.Infrastructure.Common.Validators;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biogenom.NutritionReport.Infrastructure.PersonalizedIntakes.QueryHandlers;
+
+public class PersonalizedIntakeGetQueryHandler(
+    IMapper mapper,
+    IPersonalizedIntakeService service,
+    GetQueryValidator validator)
+    : IQueryHandler<PersonalizedIntakeGetQuery, ICollection<PersonalizedIntakeGetDto>>
+{
+    public async Task<ICollection<PersonalizedIntakeGetDto>> Handle(PersonalizedIntakeGetQuery request, CancellationToken cancellationToken)
+    {
+        var filter = request.PersonalizedIntakeFilter ?? new PersonalizedIntakeFilter();
+
+        var data = await service.Get(
+                filter,
+                new QueryOptions { QueryTrackingMode = QueryTrackingMode.AsNoTracking })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<PersonalizedIntakeGetDto>>(data);
+    }
+}
