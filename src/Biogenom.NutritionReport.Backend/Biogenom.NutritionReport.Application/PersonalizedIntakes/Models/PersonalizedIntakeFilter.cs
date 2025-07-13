1dc40387dbd9dcb964554de16368c662a72f7172
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:31:58 +0500

    created PI filter model

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Application/PersonalizedIntakes/Models/PersonalizedIntakeFilter.cs
@@ -0,0 +1,17 @@
+﻿using Biogenom.NutritionReport.Domain.Common.Queries;
+
+namespace Biogenom.NutritionReport.Application.PersonalizedIntakes.Models;
+
+public class PersonalizedIntakeFilter : FilterPagination
+{
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+        hashCode.Add(PageToken);
+        hashCode.Add(PageSize);
+        return hashCode.ToHashCode();
+    }
+
+    public override bool Equals(object? obj) =>
+        obj is PersonalizedIntakeFilter filter && filter.GetHashCode() == GetHashCode();
+}
