a4c42ad8038d4e5acfce1881b5b1ddf07af359b7
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:42:41 +0500

    implemented event bus broker

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Infrastructure/Common/EventBus/Brokers/MassTransitEventBusBroker.cs
@@ -0,0 +1,21 @@
+﻿using MassTransit;
+using MediatR;
+using Biogenom.NutritionReport.Domain.Common.Events;
+using Biogenom.NutritionReport.Application.Common.EventBus.Brokers;
+
+namespace Biogenom.NutritionReport.Infrastructure.Common.EventBus.Brokers;
+
+public class MassTransitEventBusBroker(IBus bus, IPublisher publisher) : IEventBusBroker
+{
+    public async ValueTask PublishAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase =>
+        await publisher.Publish(@event, cancellationToken);
+
+    public async ValueTask PublishLocalAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase =>
+        await bus.Publish(@event, cancellationToken);
+}
---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Infrastructure/Common/EventBus/Brokers/RabbitMqEventBusBroker.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Biogenom.NutritionReport.Domain.Common.Events;
+using Biogenom.NutritionReport.Application.Common.EventBus.Brokers;
+
+namespace Biogenom.NutritionReport.Infrastructure.Common.EventBus.Brokers;
+
+public class RabbitMqEventBusBroker(IPublisher publisher) : IEventBusBroker
+{
+    public async ValueTask PublishAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase =>
+    await publisher.Publish(@event, cancellationToken);
+
+    public async ValueTask PublishLocalAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase =>
+    await publisher.Publish(@event, cancellationToken);
+}
