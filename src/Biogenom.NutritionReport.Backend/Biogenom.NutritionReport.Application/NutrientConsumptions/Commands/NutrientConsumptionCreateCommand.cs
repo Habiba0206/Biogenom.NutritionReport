28007cbe95707068539253b4b196c151ee7016ca
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:28:44 +0500

    created nutrient consumption commands

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Application/NutrientConsumptions/Commands/NutrientConsumptionCreateCommand.cs
@@ -0,0 +1,9 @@
+﻿using Biogenom.NutritionReport.Application.NutrientConsumptions.Models;
+using Biogenom.NutritionReport.Domain.Common.Commands;
+
+namespace Biogenom.NutritionReport.Application.NutrientConsumptions.Commands;
+
+public record NutrientConsumptionCreateCommand : ICommand<NutrientConsumptionCreateUpdateDto>
+{
+    public NutrientConsumptionCreateUpdateDto NutrientConsumptionCreateUpdateDto { get; set; }
+}
---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Application/NutrientConsumptions/Commands/NutrientConsumptionPatchCommand.cs
@@ -0,0 +1,9 @@
+﻿using Biogenom.NutritionReport.Application.NutrientConsumptions.Models;
+using Biogenom.NutritionReport.Domain.Common.Commands;
+
+namespace Biogenom.NutritionReport.Application.NutrientConsumptions.Commands;
+
+public record NutrientConsumptionPatchCommand : ICommand<NutrientConsumptionPatchDto>
+{
+    public NutrientConsumptionPatchDto NutrientConsumptionPatchDto { get; set; } = null!;
+}
