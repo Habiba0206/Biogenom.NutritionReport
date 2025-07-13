f0e7ee75d0a8b53c47a884589c6eeddbba6e34dc
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:33:43 +0500

    created supplement commands

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Application/Supplements/Commands/SupplementUpdateCommand.cs
@@ -0,0 +1,9 @@
+﻿using Biogenom.NutritionReport.Application.Supplements.Models;
+using Biogenom.NutritionReport.Domain.Common.Commands;
+
+namespace Biogenom.NutritionReport.Application.Supplements.Commands;
+
+public record SupplementUpdateCommand : ICommand<SupplementCreateUpdateDto>
+{
+    public SupplementCreateUpdateDto SupplementCreateUpdateDto { get; set; }
+}
