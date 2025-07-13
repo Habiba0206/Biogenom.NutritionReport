df00857dcca26dbe4c14346a2bc3ac3fab1cd5fb
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:49:44 +0500

    created app db context

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Persistance/DataContexts/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Biogenom.NutritionReport.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biogenom.NutritionReport.Persistence.DataContexts;
+
+public class AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : DbContext(dbContextOptions)
+{
+    public DbSet<Benefit> Benefits => Set<Benefit>();
+    public DbSet<NutrientConsumption> NutrientConsumptions => Set<NutrientConsumption>();
+    public DbSet<PersonalizedIntake> PersonalizedIntakes => Set<PersonalizedIntake>();
+    public DbSet<Report> Reports => Set<Report>();
+    public DbSet<Supplement> Supplements => Set<Supplement>();
+}
