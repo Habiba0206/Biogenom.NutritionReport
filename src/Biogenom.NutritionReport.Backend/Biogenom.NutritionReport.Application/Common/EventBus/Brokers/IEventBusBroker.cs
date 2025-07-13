45ae9fd0da5e8a62c0c742e2dc6e5bea1a7d8283
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:26:34 +0500

    created event bus broker

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Application/Common/EventBus/Brokers/IEventBusBroker.cs
@@ -0,0 +1,16 @@
+﻿using Biogenom.NutritionReport.Domain.Common.Events;
+
+namespace Biogenom.NutritionReport.Application.Common.EventBus.Brokers;
+
+public interface IEventBusBroker
+{
+    ValueTask PublishLocalAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase;
+
+    ValueTask PublishAsync<TEvent>(
+        TEvent @event,
+        CancellationToken cancellationToken = default)
+        where TEvent : EventBase;
+}
