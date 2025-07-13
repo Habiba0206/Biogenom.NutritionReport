2b73ffe27095f2594b88694e1e72306d6aff9c0a
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:51:09 +0500

    created NC repo

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Persistance/Repositories/Interfaces/INutrientConsumptionRepository.cs
@@ -0,0 +1,25 @@
+﻿using Biogenom.NutritionReport.Domain.Common.Commands;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+using Biogenom.NutritionReport.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Biogenom.NutritionReport.Persistence.Repositories.Interfaces;
+
+public interface INutrientConsumptionRepository
+{
+    IQueryable<NutrientConsumption> Get(Expression<Func<NutrientConsumption, bool>>? predicate = default, QueryOptions queryOptions = default);
+
+    ValueTask<NutrientConsumption?> GetByIdAsync(Guid id, QueryOptions queryOptions = default, CancellationToken cancellationToken = default);
+
+    ValueTask<bool> CheckByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    ValueTask<IList<NutrientConsumption>> GetByIdsAsync(IEnumerable<Guid> ids, QueryOptions queryOptions = default, CancellationToken cancellationToken = default);
+
+    ValueTask<NutrientConsumption> CreateAsync(NutrientConsumption entity, CommandOptions commandOptions = default, CancellationToken cancellationToken = default);
+
+    ValueTask<NutrientConsumption> UpdateAsync(NutrientConsumption entity, CommandOptions commandOptions = default, CancellationToken cancellationToken = default);
+
+    ValueTask<NutrientConsumption?> DeleteAsync(NutrientConsumption entity, CommandOptions commandOptions = default, CancellationToken cancellationToken = default);
+
+    ValueTask<NutrientConsumption?> DeleteByIdAsync(Guid id, CommandOptions commandOptions = default, CancellationToken cancellationToken = default);
+}
---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Persistance/Repositories/Interfaces/IPersonalizedIntakeRepository.cs
@@ -0,0 +1,25 @@
+﻿using Biogenom.NutritionReport.Domain.Common.Commands;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+using Biogenom.NutritionReport.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Biogenom.NutritionReport.Persistence.Repositories.Interfaces;
+
+public interface IPersonalizedIntakeRepository
+{
+    IQueryable<PersonalizedIntake> Get(Expression<Func<PersonalizedIntake, bool>>? predicate = default, QueryOptions queryOptions = default);
+
+    ValueTask<PersonalizedIntake?> GetByIdAsync(Guid id, QueryOptions queryOptions = default, CancellationToken cancellationToken = default);
+
+    ValueTask<bool> CheckByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    ValueTask<IList<PersonalizedIntake>> GetByIdsAsync(IEnumerable<Guid> ids, QueryOptions queryOptions = default, CancellationToken cancellationToken = default);
+
+    ValueTask<PersonalizedIntake> CreateAsync(PersonalizedIntake entity, CommandOptions commandOptions = default, CancellationToken cancellationToken = default);
+
+    ValueTask<PersonalizedIntake> UpdateAsync(PersonalizedIntake entity, CommandOptions commandOptions = default, CancellationToken cancellationToken = default);
+
+    ValueTask<PersonalizedIntake?> DeleteAsync(PersonalizedIntake entity, CommandOptions commandOptions = default, CancellationToken cancellationToken = default);
+
+    ValueTask<PersonalizedIntake?> DeleteByIdAsync(Guid id, CommandOptions commandOptions = default, CancellationToken cancellationToken = default);
+}
