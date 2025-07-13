b96e90d87263ca534493e97ee2890d5bd66e5340
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:46:19 +0500

    validator again

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Infrastructure/PersonalizedIntakes/Validators/PersonalizedIntakeValidator.cs
@@ -0,0 +1,24 @@
+﻿using Biogenom.NutritionReport.Application.PersonalizedIntakes.Models;
+using FluentValidation;
+
+namespace Biogenom.NutritionReport.Infrastructure.PersonalizedIntakes.Validators;
+
+public class PersonalizedIntakeValidator : AbstractValidator<PersonalizedIntakeCreateUpdateDto>
+{
+    public PersonalizedIntakeValidator()
+    {
+        RuleFor(x => x.NutrientName)
+            .NotEmpty().WithMessage("Nutrient name is required.");
+
+        RuleFor(x => x.Unit)
+            .NotEmpty().WithMessage("Unit is required.");
+
+        RuleFor(x => x.NewValue)
+            .GreaterThanOrEqualTo(0).WithMessage("New value must be non‑negative.");
+
+        RuleFor(x => x.Source)
+            .NotEmpty().WithMessage("Source is required.")
+            .Must(s => s is "Food" or "Kit" or "Total")
+            .WithMessage("Source must be Food, Kit, or Total.");
+    }
+}
