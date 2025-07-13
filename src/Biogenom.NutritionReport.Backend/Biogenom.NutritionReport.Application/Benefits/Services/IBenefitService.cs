5c3924a4499e9ed256d4b3bffb8e67db4a78e213
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:25:31 +0500

    created benefit service

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Application/Benefits/Services/IBenefitService.cs
@@ -0,0 +1,57 @@
+﻿using Biogenom.NutritionReport.Application.Benefits.Models;
+using Biogenom.NutritionReport.Domain.Common.Commands;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+using Biogenom.NutritionReport.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Biogenom.NutritionReport.Application.Benefits.Services;
+
+public interface IBenefitService
+{
+    IQueryable<Benefit> Get(
+        Expression<Func<Benefit, bool>>? predicate = default, 
+        QueryOptions queryOptions = default);
+    
+    IQueryable<Benefit> Get(
+        BenefitFilter filter, 
+        QueryOptions queryOptions = default);
+    
+    ValueTask<Benefit?> GetByIdAsync(
+        Guid id, 
+        QueryOptions queryOptions = default, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<bool> CheckByIdAsync(
+        Guid id, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<IList<Benefit>> GetByIdsAsync(
+        IEnumerable<Guid> ids, 
+        QueryOptions queryOptions = default, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<Benefit> CreateAsync(
+        Benefit entity, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<Benefit> UpdateAsync(
+        Benefit entity, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<Benefit> PatchAsync(
+        BenefitPatchDto patchDto, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<Benefit?> DeleteAsync(
+        Benefit entity, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default);
+    
+    ValueTask<Benefit?> DeleteByIdAsync(
+        Guid id, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default);
+}
