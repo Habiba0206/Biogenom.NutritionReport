cf17565e2c089651cf4e07aae8066a3b2a014025
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:24:41 +0500

    created benefit filter

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Application/Benefits/Models/BenefitFilter.cs
@@ -0,0 +1,17 @@
+﻿using Biogenom.NutritionReport.Domain.Common.Queries;
+
+namespace Biogenom.NutritionReport.Application.Benefits.Models;
+
+public class BenefitFilter : FilterPagination
+{
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+        hashCode.Add(PageToken);
+        hashCode.Add(PageSize);
+        return hashCode.ToHashCode();
+    }
+
+    public override bool Equals(object? obj) =>
+        obj is BenefitFilter filter && filter.GetHashCode() == GetHashCode();
+}
