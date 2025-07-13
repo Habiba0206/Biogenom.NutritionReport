b5fe28440a44c28b1ed2366cefe65098c00426fe
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:48:13 +0500

    created supplement mapper

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Infrastructure/Supplements/Mappers/SupplementMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Biogenom.NutritionReport.Application.Supplements.Models;
+using Biogenom.NutritionReport.Domain.Entities;
+
+namespace Biogenom.NutritionReport.Infrastructure.Supplements.Mappers;
+
+public class SupplementMapper : Profile
+{
+    public SupplementMapper()
+    {
+        CreateMap<Supplement, SupplementCreateUpdateDto>().ReverseMap();
+        CreateMap<Supplement, SupplementGetDto>().ReverseMap();
+        CreateMap<Supplement, SupplementPatchDto>().ReverseMap();
+    }
+}
