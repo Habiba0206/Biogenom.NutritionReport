f4ec4415e37df1241e2f56e2901fbb6d4f448959
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:33:11 +0500

    created report filter model

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Application/Reports/Models/ReportFilter.cs
@@ -0,0 +1,17 @@
+﻿using Biogenom.NutritionReport.Domain.Common.Queries;
+
+namespace Biogenom.NutritionReport.Application.Reports.Models;
+
+public class ReportFilter : FilterPagination
+{
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+        hashCode.Add(PageToken);
+        hashCode.Add(PageSize);
+        return hashCode.ToHashCode();
+    }
+
+    public override bool Equals(object? obj) =>
+        obj is ReportFilter filter && filter.GetHashCode() == GetHashCode();
+}
