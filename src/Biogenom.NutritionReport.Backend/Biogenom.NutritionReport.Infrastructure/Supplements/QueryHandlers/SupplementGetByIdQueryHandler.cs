f8f0ccb62cece41f286eee781752215952500bb0
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:48:25 +0500

    created supplemented query handlers

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Infrastructure/Supplements/QueryHandlers/SupplementGetByIdQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Biogenom.NutritionReport.Application.Supplements.Models;
+using Biogenom.NutritionReport.Application.Supplements.Queries;
+using Biogenom.NutritionReport.Application.Supplements.Services;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+
+namespace Biogenom.NutritionReport.Infrastructure.Supplements.QueryHandlers;
+
+public class SupplementGetByIdQueryHandler(
+    IMapper mapper,
+    ISupplementService service)
+    : IQueryHandler<SupplementGetByIdQuery, SupplementGetDto?>
+{
+    public async Task<SupplementGetDto?> Handle(SupplementGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var entity = await service.GetByIdAsync(request.SupplementId, cancellationToken: cancellationToken);
+        return mapper.Map<SupplementGetDto?>(entity);
+    }
+}
---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Infrastructure/Supplements/QueryHandlers/SupplementGetQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Biogenom.NutritionReport.Application.Supplements.Models;
+using Biogenom.NutritionReport.Application.Supplements.Queries;
+using Biogenom.NutritionReport.Application.Supplements.Services;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+using Biogenom.NutritionReport.Infrastructure.Common.Validators;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biogenom.NutritionReport.Infrastructure.Supplements.QueryHandlers;
+
+public class SupplementGetQueryHandler(
+    IMapper mapper,
+    ISupplementService service,
+    GetQueryValidator validator)
+    : IQueryHandler<SupplementGetQuery, ICollection<SupplementGetDto>>
+{
+    public async Task<ICollection<SupplementGetDto>> Handle(SupplementGetQuery request, CancellationToken cancellationToken)
+    {
+        var filter = request.SupplementFilter ?? new SupplementFilter();
+        
+        var data = await service.Get(
+                filter,
+                new QueryOptions { QueryTrackingMode = QueryTrackingMode.AsNoTracking })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<SupplementGetDto>>(data);
+    }
+}
