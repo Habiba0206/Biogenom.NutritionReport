d347edb61089b1532331bc7d1cb0579e41f4be20
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:47:04 +0500

    created report query handlers

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Infrastructure/Reports/QueryHandlers/ReportGetByIdQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Biogenom.NutritionReport.Application.Reports.Models;
+using Biogenom.NutritionReport.Application.Reports.Queries;
+using Biogenom.NutritionReport.Application.Reports.Services;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+
+namespace Biogenom.NutritionReport.Infrastructure.Reports.QueryHandlers;
+
+public class ReportGetByIdQueryHandler(
+    IMapper mapper,
+    IReportService service)
+    : IQueryHandler<ReportGetByIdQuery, ReportGetDto?>
+{
+    public async Task<ReportGetDto?> Handle(ReportGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var entity = await service.GetByIdAsync(request.ReportId, cancellationToken: cancellationToken);
+        return mapper.Map<ReportGetDto?>(entity);
+    }
+}
---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Infrastructure/Reports/QueryHandlers/ReportGetQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Biogenom.NutritionReport.Application.Reports.Models;
+using Biogenom.NutritionReport.Application.Reports.Queries;
+using Biogenom.NutritionReport.Application.Reports.Services;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+using Biogenom.NutritionReport.Infrastructure.Common.Validators;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biogenom.NutritionReport.Infrastructure.Reports.QueryHandlers;
+
+public class ReportGetQueryHandler(
+    IMapper mapper,
+    IReportService service,
+    GetQueryValidator validator)
+    : IQueryHandler<ReportGetQuery, ICollection<ReportGetDto>>
+{
+    public async Task<ICollection<ReportGetDto>> Handle(ReportGetQuery request, CancellationToken cancellationToken)
+    {
+        var filter = request.ReportFilter ?? new ReportFilter();
+        
+        var data = await service.Get(
+                filter,
+                new QueryOptions { QueryTrackingMode = QueryTrackingMode.AsNoTracking })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<ReportGetDto>>(data);
+    }
+}
