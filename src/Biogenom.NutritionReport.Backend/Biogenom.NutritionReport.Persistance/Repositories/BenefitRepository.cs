c49660fedae2b90ea274287065694260caeb03c1
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:51:01 +0500

    created benefit repo

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Persistance/Repositories/BenefitRepository.cs
@@ -0,0 +1,40 @@
+﻿using Biogenom.NutritionReport.Domain.Common.Commands;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+using Biogenom.NutritionReport.Domain.Entities;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Biogenom.NutritionReport.Persistence.Caching.Brokers;
+using Biogenom.NutritionReport.Persistence.DataContexts;
+using Biogenom.NutritionReport.Persistence.Repositories.Interfaces;
+
+namespace Biogenom.NutritionReport.Persistence.Repositories;
+
+public class BenefitRepository(AppDbContext db, ICacheBroker cacheBroker)
+    : EntityRepositoryBase<Benefit, AppDbContext>(db, cacheBroker),
+      IBenefitRepository
+{
+    public IQueryable<Benefit> Get(Expression<Func<Benefit, bool>>? predicate = null, QueryOptions queryOptions = default) =>
+        base.Get(predicate, queryOptions)
+            .Include(b => b.Report);
+
+    public ValueTask<Benefit?> GetByIdAsync(Guid id, QueryOptions queryOptions = default, CancellationToken ct = default) =>
+        base.GetByIdAsync(id, queryOptions, ct);
+
+    public ValueTask<IList<Benefit>> GetByIdsAsync(IEnumerable<Guid> ids, QueryOptions queryOptions = default, CancellationToken ct = default) =>
+        base.GetByIdsAsync(ids, queryOptions, ct);
+
+    public ValueTask<bool> CheckByIdAsync(Guid id, CancellationToken ct = default) =>
+        base.CheckByIdAsync(id, ct);
+
+    public ValueTask<Benefit> CreateAsync(Benefit entity, CommandOptions cmd = default, CancellationToken ct = default) =>
+        base.CreateAsync(entity, cmd, ct);
+
+    public ValueTask<Benefit> UpdateAsync(Benefit entity, CommandOptions cmd, CancellationToken ct) =>
+        base.UpdateAsync(entity, cmd, ct);
+
+    public ValueTask<Benefit?> DeleteAsync(Benefit entity, CommandOptions cmd, CancellationToken ct = default) =>
+        base.DeleteAsync(entity, cmd, ct);
+
+    public ValueTask<Benefit?> DeleteByIdAsync(Guid id, CommandOptions cmd, CancellationToken ct = default) =>
+        base.DeleteByIdAsync(id, cmd, ct);
+}
---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Persistance/Repositories/Interfaces/IBenefitRepository.cs
@@ -0,0 +1,25 @@
+﻿using Biogenom.NutritionReport.Domain.Common.Commands;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+using Biogenom.NutritionReport.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Biogenom.NutritionReport.Persistence.Repositories.Interfaces;
+
+public interface IBenefitRepository
+{
+    IQueryable<Benefit> Get(Expression<Func<Benefit, bool>>? predicate = default, QueryOptions queryOptions = default);
+
+    ValueTask<Benefit?> GetByIdAsync(Guid id, QueryOptions queryOptions = default, CancellationToken cancellationToken = default);
+
+    ValueTask<bool> CheckByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    ValueTask<IList<Benefit>> GetByIdsAsync(IEnumerable<Guid> ids, QueryOptions queryOptions = default, CancellationToken cancellationToken = default);
+
+    ValueTask<Benefit> CreateAsync(Benefit entity, CommandOptions commandOptions = default, CancellationToken cancellationToken = default);
+
+    ValueTask<Benefit> UpdateAsync(Benefit entity, CommandOptions commandOptions = default, CancellationToken cancellationToken = default);
+
+    ValueTask<Benefit?> DeleteAsync(Benefit entity, CommandOptions commandOptions = default, CancellationToken cancellationToken = default);
+
+    ValueTask<Benefit?> DeleteByIdAsync(Guid id, CommandOptions commandOptions = default, CancellationToken cancellationToken = default);
+}
