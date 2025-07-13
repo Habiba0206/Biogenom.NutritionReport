ab231177ffa8d35a6bb250e76760e5c306d86e33
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:34:54 +0500

    created supplement queries

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Application/Supplements/Queries/SupplementGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using Biogenom.NutritionReport.Application.Supplements.Models;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+
+namespace Biogenom.NutritionReport.Application.Supplements.Queries;
+
+public record SupplementGetByIdQuery : IQuery<SupplementGetDto?>
+{
+    public Guid SupplementId { get; set; }
+}
