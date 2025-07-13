f3a79362c9b6ac41637cd5f505f6dd930b028c8b
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:41:09 +0500

    implemented benefit service

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Infrastructure/Benefits/Services/BenefitService.cs
@@ -0,0 +1,85 @@
+﻿using Biogenom.NutritionReport.Application.Benefits.Models;
+using Biogenom.NutritionReport.Application.Benefits.Services;
+using Biogenom.NutritionReport.Domain.Common.Commands;
+using Biogenom.NutritionReport.Domain.Common.Exceptions;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+using Biogenom.NutritionReport.Domain.Entities;
+using Biogenom.NutritionReport.Persistence.Extensions;
+using Biogenom.NutritionReport.Persistence.Repositories.Interfaces;
+using System.Linq.Expressions;
+
+namespace Biogenom.NutritionReport.Infrastructure.Benefits.Services;
+
+public class BenefitService(IBenefitRepository repository) : IBenefitService
+{
+    public IQueryable<Benefit> Get(
+        Expression<Func<Benefit, bool>>? predicate = null, 
+        QueryOptions queryOptions = default) =>
+        repository.Get(predicate, queryOptions);
+
+    public IQueryable<Benefit> Get(
+        BenefitFilter filter, 
+        QueryOptions queryOptions = default) =>
+        repository.Get(queryOptions: queryOptions)
+        .ApplyPagination(filter);
+
+    public ValueTask<Benefit?> GetByIdAsync(
+        Guid id, 
+        QueryOptions queryOptions = default, 
+        CancellationToken cancellationToken = default) =>
+        repository.GetByIdAsync(id, queryOptions, cancellationToken);
+
+    public ValueTask<IList<Benefit>> GetByIdsAsync(
+        IEnumerable<Guid> ids, 
+        QueryOptions queryOptions = default,
+        CancellationToken cancellationToken = default) =>
+        repository.GetByIdsAsync(ids, queryOptions, cancellationToken);
+
+    public ValueTask<bool> CheckByIdAsync(
+        Guid id, 
+        CancellationToken cancellationToken = default) =>
+        repository.CheckByIdAsync(id, cancellationToken);
+
+    public ValueTask<Benefit> CreateAsync(
+        Benefit entity, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default) =>
+        repository.CreateAsync(entity, commandOptions, cancellationToken);
+
+    public async ValueTask<Benefit> UpdateAsync(
+        Benefit entity, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default)
+    {
+        var existing = await repository.GetByIdAsync(entity.Id) ?? throw new NotFoundException(nameof(Benefit), entity.Id);
+
+        existing.Text = entity.Text;
+
+        return await repository.UpdateAsync(existing, commandOptions, cancellationToken);
+    }
+
+    public async ValueTask<Benefit> PatchAsync(
+        BenefitPatchDto patchDto, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default)
+    {
+        var existing = await repository.GetByIdAsync(patchDto.Id, cancellationToken: cancellationToken) 
+            ?? throw new NotFoundException(nameof(Benefit), patchDto.Id);
+        
+        if (patchDto.Text is not null) existing.Text = patchDto.Text;
+        
+        return await repository.UpdateAsync(existing, cancellationToken: cancellationToken);
+    }
+
+    public ValueTask<Benefit?> DeleteAsync(
+        Benefit entity, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default) =>
+        repository.DeleteAsync(entity, commandOptions, cancellationToken);
+
+    public ValueTask<Benefit?> DeleteByIdAsync(
+        Guid id, 
+        CommandOptions commandOptions = default, 
+        CancellationToken cancellationToken = default) =>
+        repository.DeleteByIdAsync(id, commandOptions, cancellationToken);
+}
