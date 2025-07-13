0c88ca0e37fc72f1ff8afc09f210d39a68d22d03
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:40:56 +0500

    created benefit query handlers

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Infrastructure/Benefits/QueryHandlers/BenefitGetByIdQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Biogenom.NutritionReport.Application.Benefits.Models;
+using Biogenom.NutritionReport.Application.Benefits.Queries;
+using Biogenom.NutritionReport.Application.Benefits.Services;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+
+namespace Biogenom.NutritionReport.Infrastructure.Benefits.QueryHandlers;
+
+public class BenefitGetByIdQueryHandler(
+    IMapper mapper,
+    IBenefitService service)
+    : IQueryHandler<BenefitGetByIdQuery, BenefitGetDto?>
+{
+    public async Task<BenefitGetDto?> Handle(BenefitGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var entity = await service.GetByIdAsync(request.BenefitId, cancellationToken: cancellationToken);
+        return mapper.Map<BenefitGetDto?>(entity);
+    }
+}
---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Infrastructure/Benefits/QueryHandlers/BenefitGetQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Biogenom.NutritionReport.Application.Benefits.Models;
+using Biogenom.NutritionReport.Application.Benefits.Queries;
+using Biogenom.NutritionReport.Application.Benefits.Services;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+using Biogenom.NutritionReport.Infrastructure.Common.Validators;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biogenom.NutritionReport.Infrastructure.Benefits.QueryHandlers;
+
+public class BenefitGetQueryHandler(
+    IMapper mapper,
+    IBenefitService service,
+    GetQueryValidator validator)
+    : IQueryHandler<BenefitGetQuery, ICollection<BenefitGetDto>>
+{
+    public async Task<ICollection<BenefitGetDto>> Handle(BenefitGetQuery request, CancellationToken cancellationToken)
+    {
+        var filter = request.BenefitFilter ?? new BenefitFilter();
+        
+        var data = await service.Get(
+                filter,
+                new QueryOptions { QueryTrackingMode = QueryTrackingMode.AsNoTracking })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<BenefitGetDto>>(data);
+    }
+}
