be3ce9e0828c495f06ce78de1c78d93b5d878c0f
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:44:30 +0500

    created NC query handlers

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Infrastructure/NutrientConsumptions/QueryHandlers/NutrientConsumptionGetByIdQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Biogenom.NutritionReport.Application.NutrientConsumptions.Models;
+using Biogenom.NutritionReport.Application.NutrientConsumptions.Queries;
+using Biogenom.NutritionReport.Application.NutrientConsumptions.Services;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+
+namespace Biogenom.NutritionReport.Infrastructure.NutrientConsumptions.QueryHandlers;
+
+public class NutrientConsumptionGetByIdQueryHandler(
+    IMapper mapper,
+    INutrientConsumptionService service)
+    : IQueryHandler<NutrientConsumptionGetByIdQuery, NutrientConsumptionGetDto?>
+{
+    public async Task<NutrientConsumptionGetDto?> Handle(NutrientConsumptionGetByIdQuery request, CancellationToken cancellationToken)
+    {
+        var entity = await service.GetByIdAsync(request.NutrientConsumptionId, cancellationToken: cancellationToken);
+        return mapper.Map<NutrientConsumptionGetDto?>(entity);
+    }
+}
---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Infrastructure/NutrientConsumptions/QueryHandlers/NutrientConsumptionGetQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Biogenom.NutritionReport.Application.NutrientConsumptions.Models;
+using Biogenom.NutritionReport.Application.NutrientConsumptions.Queries;
+using Biogenom.NutritionReport.Application.NutrientConsumptions.Services;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+using Biogenom.NutritionReport.Infrastructure.Common.Validators;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biogenom.NutritionReport.Infrastructure.NutrientConsumptions.QueryHandlers;
+
+public class NutrientConsumptionGetQueryHandler(
+    IMapper mapper,
+    INutrientConsumptionService service,
+    GetQueryValidator validator)
+    : IQueryHandler<NutrientConsumptionGetQuery, ICollection<NutrientConsumptionGetDto>>
+{
+    public async Task<ICollection<NutrientConsumptionGetDto>> Handle(NutrientConsumptionGetQuery request, CancellationToken cancellationToken)
+    {
+        var filter = request.NutrientConsumptionFilter ?? new NutrientConsumptionFilter();
+        
+        var data = await service.Get(
+                filter,
+                new QueryOptions { QueryTrackingMode = QueryTrackingMode.AsNoTracking })
+            .ToListAsync(cancellationToken);
+
+        return mapper.Map<ICollection<NutrientConsumptionGetDto>>(data);
+    }
+}
