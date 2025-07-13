3dd4bb2e61ded5c108235351430f50068a584c77
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:44:18 +0500

    created NC mapper

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Infrastructure/NutrientConsumptions/Mappers/NutrientConsumptionMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Biogenom.NutritionReport.Application.NutrientConsumptions.Models;
+using Biogenom.NutritionReport.Domain.Entities;
+
+namespace Biogenom.NutritionReport.Infrastructure.NutrientConsumptions.Mappers;
+
+public class NutrientConsumptionMapper : Profile
+{
+    public NutrientConsumptionMapper()
+    {
+        CreateMap<NutrientConsumption, NutrientConsumptionCreateUpdateDto>().ReverseMap();
+        CreateMap<NutrientConsumption, NutrientConsumptionGetDto>().ReverseMap();
+        CreateMap<NutrientConsumption, NutrientConsumptionPatchDto>().ReverseMap();
+    }
+}
