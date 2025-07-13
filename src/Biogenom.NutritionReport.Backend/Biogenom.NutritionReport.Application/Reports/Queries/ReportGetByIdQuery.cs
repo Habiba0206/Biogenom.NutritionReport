47896c277bf2387b09ca8f03a9d06df341df7609
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:33:22 +0500

    created report queries

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Application/Reports/Queries/ReportGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using Biogenom.NutritionReport.Application.Reports.Models;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+
+namespace Biogenom.NutritionReport.Application.Reports.Queries;
+
+public record ReportGetByIdQuery : IQuery<ReportGetDto?>
+{
+    public Guid ReportId { get; set; }
+}
---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Application/Reports/Queries/ReportGetQuery.cs
@@ -0,0 +1,9 @@
+﻿using Biogenom.NutritionReport.Application.Reports.Models;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+
+namespace Biogenom.NutritionReport.Application.Reports.Queries;
+
+public record ReportGetQuery : IQuery<ICollection<ReportGetDto>>
+{
+    public ReportFilter ReportFilter { get; set; }
+}
