6556506441bd8c7a92cd404db93ec450322e44e8
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:40:44 +0500

    created benefit mapper

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Infrastructure/Benefits/Mappers/BenefitMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Biogenom.NutritionReport.Application.Benefits.Models;
+using Biogenom.NutritionReport.Domain.Entities;
+
+namespace Biogenom.NutritionReport.Infrastructure.Benefits.Mappers;
+
+public class BenefitMapper : Profile
+{
+    public BenefitMapper()
+    {
+        CreateMap<Benefit, BenefitCreateUpdateDto>().ReverseMap();
+        CreateMap<Benefit, BenefitGetDto>().ReverseMap();
+        CreateMap<Benefit, BenefitPatchDto>().ReverseMap();
+    }
+}
