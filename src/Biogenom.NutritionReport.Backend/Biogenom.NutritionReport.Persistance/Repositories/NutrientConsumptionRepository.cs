5263296b5132360d988528de0fa73c9f281d67e6
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:51:52 +0500

    created PI repo

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Persistance/Repositories/NutrientConsumptionRepository.cs
@@ -0,0 +1,41 @@
+﻿using Biogenom.NutritionReport.Domain.Common.Commands;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+using Biogenom.NutritionReport.Domain.Entities;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Biogenom.NutritionReport.Persistence.Caching.Brokers;
+using Biogenom.NutritionReport.Persistence.DataContexts;
+using Biogenom.NutritionReport.Persistence.Repositories.Interfaces;
+
+namespace Biogenom.NutritionReport.Persistence.Repositories;
+
+public class NutrientConsumptionRepository(AppDbContext db, ICacheBroker cacheBroker)
+    : EntityRepositoryBase<NutrientConsumption, AppDbContext>(db, cacheBroker),
+      INutrientConsumptionRepository
+{
+    public IQueryable<NutrientConsumption> Get(Expression<Func<NutrientConsumption, bool>>? predicate = null, QueryOptions queryOptions = default) =>
+        base.Get(predicate, queryOptions)
+            .Include(n => n.Report);
+
+    public ValueTask<NutrientConsumption?> GetByIdAsync(Guid id, QueryOptions queryOptions = default, CancellationToken ct = default) =>
+       new(Get(r => r.Id == id, queryOptions)
+       .FirstOrDefaultAsync(ct));
+
+    public ValueTask<IList<NutrientConsumption>> GetByIdsAsync(IEnumerable<Guid> ids, QueryOptions queryOptions = default, CancellationToken ct = default) =>
+        base.GetByIdsAsync(ids, queryOptions, ct);
+
+    public ValueTask<bool> CheckByIdAsync(Guid id, CancellationToken ct = default) =>
+        base.CheckByIdAsync(id, ct);
+
+    public ValueTask<NutrientConsumption> CreateAsync(NutrientConsumption entity, CommandOptions cmd = default, CancellationToken ct = default) =>
+        base.CreateAsync(entity, cmd, ct);
+
+    public ValueTask<NutrientConsumption> UpdateAsync(NutrientConsumption entity, CommandOptions cmd, CancellationToken ct) =>
+        base.UpdateAsync(entity, cmd, ct);
+
+    public ValueTask<NutrientConsumption?> DeleteAsync(NutrientConsumption entity, CommandOptions cmd, CancellationToken ct = default) =>
+        base.DeleteAsync(entity, cmd, ct);
+
+    public ValueTask<NutrientConsumption?> DeleteByIdAsync(Guid id, CommandOptions cmd, CancellationToken ct = default) =>
+        base.DeleteByIdAsync(id, cmd, ct);
+}
---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Persistance/Repositories/PersonalizedIntakeRepository.cs
@@ -0,0 +1,41 @@
+﻿using Biogenom.NutritionReport.Domain.Common.Commands;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+using Biogenom.NutritionReport.Domain.Entities;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Biogenom.NutritionReport.Persistence.Caching.Brokers;
+using Biogenom.NutritionReport.Persistence.DataContexts;
+using Biogenom.NutritionReport.Persistence.Repositories.Interfaces;
+
+namespace Biogenom.NutritionReport.Persistence.Repositories;
+
+public class PersonalizedIntakeRepository(AppDbContext db, ICacheBroker cacheBroker)
+    : EntityRepositoryBase<PersonalizedIntake, AppDbContext>(db, cacheBroker),
+      IPersonalizedIntakeRepository
+{
+    public IQueryable<PersonalizedIntake> Get(Expression<Func<PersonalizedIntake, bool>>? predicate = null, QueryOptions queryOptions = default) =>
+        base.Get(predicate, queryOptions)
+            .Include(p => p.Report);
+
+    public ValueTask<PersonalizedIntake?> GetByIdAsync(Guid id, QueryOptions queryOptions = default, CancellationToken ct = default) =>
+        new(Get(r => r.Id == id, queryOptions)
+       .FirstOrDefaultAsync(ct));
+
+    public ValueTask<IList<PersonalizedIntake>> GetByIdsAsync(IEnumerable<Guid> ids, QueryOptions queryOptions = default, CancellationToken ct = default) =>
+        base.GetByIdsAsync(ids, queryOptions, ct);
+
+    public ValueTask<bool> CheckByIdAsync(Guid id, CancellationToken ct = default) =>
+        base.CheckByIdAsync(id, ct);
+
+    public ValueTask<PersonalizedIntake> CreateAsync(PersonalizedIntake entity, CommandOptions cmd = default, CancellationToken ct = default) =>
+        base.CreateAsync(entity, cmd, ct);
+
+    public ValueTask<PersonalizedIntake> UpdateAsync(PersonalizedIntake entity, CommandOptions cmd, CancellationToken ct) =>
+        base.UpdateAsync(entity, cmd, ct);
+
+    public ValueTask<PersonalizedIntake?> DeleteAsync(PersonalizedIntake entity, CommandOptions cmd, CancellationToken ct = default) =>
+        base.DeleteAsync(entity, cmd, ct);
+
+    public ValueTask<PersonalizedIntake?> DeleteByIdAsync(Guid id, CommandOptions cmd, CancellationToken ct = default) =>
+        base.DeleteByIdAsync(id, cmd, ct);
+}
