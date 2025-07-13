9f1d11e122516771c9f95914e1e8b8a70fe2bb7b
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:32:08 +0500

    created PI queries

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Application/PersonalizedIntakes/Queries/PersonalizedIntakeGetByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using Biogenom.NutritionReport.Application.PersonalizedIntakes.Models;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+
+namespace Biogenom.NutritionReport.Application.PersonalizedIntakes.Queries;
+
+public record PersonalizedIntakeGetByIdQuery : IQuery<PersonalizedIntakeGetDto?>
+{
+    public Guid PersonalizedIntakeId { get; set; }
+}
---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Application/PersonalizedIntakes/Queries/PersonalizedIntakeGetQuery.cs
@@ -0,0 +1,9 @@
+﻿using Biogenom.NutritionReport.Application.PersonalizedIntakes.Models;
+using Biogenom.NutritionReport.Domain.Common.Queries;
+
+namespace Biogenom.NutritionReport.Application.PersonalizedIntakes.Queries;
+
+public record PersonalizedIntakeGetQuery : IQuery<ICollection<PersonalizedIntakeGetDto>>
+{
+    public PersonalizedIntakeFilter PersonalizedIntakeFilter { get; set; }
+}
