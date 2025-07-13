cb26db4ee376d48be9b22455c6c9ff97c35562e1
Habiba Sattorova <habibasattorova26@gmail.com>
2025-07-13 18:46:52 +0500

    created report mapper

---
+++ src/Biogenom.NutritionReport.Backend/Biogenom.NutritionReport.Infrastructure/Reports/Mappers/ReportMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Biogenom.NutritionReport.Application.Reports.Models;
+using Biogenom.NutritionReport.Domain.Entities;
+
+namespace Biogenom.NutritionReport.Infrastructure.Reports.Mappers;
+
+public class ReportMapper : Profile
+{
+    public ReportMapper()
+    {
+        CreateMap<Report, ReportCreateUpdateDto>().ReverseMap();
+        CreateMap<Report, ReportGetDto>().ReverseMap();
+        CreateMap<Report, ReportPatchDto>().ReverseMap();
+    }
+}
